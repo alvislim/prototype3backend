cc0c6f6f94c0437bdd97e736a3ae73d22fc2fbe6
alvislim <alvislws@gmail.com>
2020-07-27 15:54:56 +0800

    amended cors origin url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors({ origin: "https://gap3frontend.herokuapp.com/", credentials: true }));
+app.use(cors({ origin: "https://gap3frontend.herokuapp.com", credentials: true }));
 
 app.use(
     session({
